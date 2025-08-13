aa89c670554604cea476921e70a674e8625478a8
Zino <zinoakpoghelie17@gmail.com>
2025-08-13 09:24:35 +0100

    update

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuIcon = document.getElementById("menu");
   const closeIcon = document.getElementById("close");
   const navList = document.querySelector(".nav-links ul");
-
   const navLinks = navList.querySelectorAll("a");
 
   const closeMobileMenu = () => {
@@ -47,11 +46,50 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault(); // stop immediate jump
+      const href = link.getAttribute("href");
       closeMobileMenu();
+      setTimeout(() => {
+        window.location.href = href;
+      }, 100); // tiny delay for Android
     });
   });
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const menuIcon = document.getElementById("menu");
+//   const closeIcon = document.getElementById("close");
+//   const navList = document.querySelector(".nav-links ul");
+
+//   const navLinks = navList.querySelectorAll("a");
+
+//   const closeMobileMenu = () => {
+//     navList.classList.remove("show");
+//     closeIcon.style.display = "none";
+//     menuIcon.style.display = "block";
+//   };
+
+//   menuIcon.addEventListener("click", () => {
+//     navList.classList.add("show");
+//     menuIcon.style.display = "none";
+//     closeIcon.style.display = "block";
+//   });
+
+//   closeIcon.addEventListener("click", () => {
+//     closeMobileMenu();
+//   });
+
+//   navLinks.forEach((link) => {
+//     link.addEventListener("click", () => {
+//       closeMobileMenu();
+//     });
+//   });
+// });
+
+window.onload = function () {
+  window.scrollTo(0, 0);
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver((entries) => {
